2de3d59182af4d5ea5582533a518f52339c5f47d
Denis T <dragon-dreamer@users.noreply.github.com>
2022-05-13 00:44:18 +0100

    Updated COMPID database

--- pe_bliss2/rich/compid_database.cpp
+++ pe_bliss2/rich/compid_database.cpp
@@ -338,6 +338,7 @@ constexpr std::array products
 	product_mapping{ 0x0ff, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x0ff, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
 	product_mapping{ 0x0ff, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x0ff, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 },
 	product_mapping{ 0x100, 23026, product_type::visual_studio_2015 },
 	product_mapping{ 0x100, 23506, product_type::visual_studio_2015_update1 },
 	product_mapping{ 0x100, 23918, product_type::visual_studio_2015_update2 },
@@ -416,6 +417,7 @@ constexpr std::array products
 	product_mapping{ 0x100, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x100, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
 	product_mapping{ 0x100, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x100, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 },
 	product_mapping{ 0x101, 23026, product_type::visual_studio_2015 },
 	product_mapping{ 0x101, 23506, product_type::visual_studio_2015_update1 },
 	product_mapping{ 0x101, 23918, product_type::visual_studio_2015_update2 },
@@ -494,6 +496,7 @@ constexpr std::array products
 	product_mapping{ 0x101, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x101, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
 	product_mapping{ 0x101, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x101, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 },
 	product_mapping{ 0x102, 23026, product_type::visual_studio_2015 },
 	product_mapping{ 0x102, 23506, product_type::visual_studio_2015_update1 },
 	product_mapping{ 0x102, 23918, product_type::visual_studio_2015_update2 },
@@ -572,6 +575,7 @@ constexpr std::array products
 	product_mapping{ 0x102, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x102, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
 	product_mapping{ 0x102, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x102, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 },
 	product_mapping{ 0x103, 23026, product_type::visual_studio_2015 },
 	product_mapping{ 0x103, 23506, product_type::visual_studio_2015_update1 },
 	product_mapping{ 0x103, 23918, product_type::visual_studio_2015_update2 },
@@ -649,6 +653,7 @@ constexpr std::array products
 	product_mapping{ 0x103, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x103, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
 	product_mapping{ 0x103, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x103, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 },
 	product_mapping{ 0x104, 23026, product_type::visual_studio_2015 },
 	product_mapping{ 0x104, 23506, product_type::visual_studio_2015_update1 },
 	product_mapping{ 0x104, 23918, product_type::visual_studio_2015_update2 },
@@ -727,6 +732,7 @@ constexpr std::array products
 	product_mapping{ 0x104, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x104, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
 	product_mapping{ 0x104, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x104, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 },
 	product_mapping{ 0x105, 23026, product_type::visual_studio_2015 },
 	product_mapping{ 0x105, 23506, product_type::visual_studio_2015_update1 },
 	product_mapping{ 0x105, 23918, product_type::visual_studio_2015_update2 },
@@ -804,7 +810,8 @@ constexpr std::array products
 	product_mapping{ 0x105, 31114, product_type::visual_studio_2022_17_2_0_preview1_0 },
 	product_mapping{ 0x105, 31302, product_type::visual_studio_2022_17_2_0_preview2_1 },
 	product_mapping{ 0x105, 31326, product_type::visual_studio_2022_17_2_0_preview3_0 },
-	product_mapping{ 0x105, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 }
+	product_mapping{ 0x105, 31328, product_type::visual_studio_2022_17_2_0_preview5_0 },
+	product_mapping{ 0x105, 31424, product_type::visual_studio_2022_17_3_0_preview1_0 }
 };
 
 static_assert(std::is_sorted(products.cbegin(), products.cend()),
@@ -1037,6 +1044,7 @@ const char* compid_database::product_type_to_string(product_type type) noexcept
 	case visual_studio_2022_17_2_0_preview2_1: return "Visual Studio 2022 17.2.0 Preview 2.1";
 	case visual_studio_2022_17_2_0_preview3_0: return "Visual Studio 2022 17.2.0 Preview 3.0";
 	case visual_studio_2022_17_2_0_preview5_0: return "Visual Studio 2022 17.2.0 Preview 5.0";
+	case visual_studio_2022_17_3_0_preview1_0: return "Visual Studio 2022 17.3.0 Preview 1.0";
 	case unmarked_object: return "Unmarked object";
 	default: return "Unknown";
 	}
