332c8ca67bc9464b3c76d479a5133ab9008d5cf1
Denis T <dragon-dreamer@users.noreply.github.com>
2023-04-24 19:17:40 +0100

    Minor image_delayload_descriptor changes

--- pe_bliss2/detail/delay_import/image_delay_load_descriptor.h
+++ pe_bliss2/detail/delay_import/image_delay_load_descriptor.h
@@ -2,14 +2,10 @@
 
 #include <cstdint>
 
-#include "pe_bliss2/detail/concepts.h"
 #include "pe_bliss2/pe_types.h"
 
 namespace pe_bliss::detail::delay_import
 {
-
-template<executable_pointer Pointer>
-using image_thunk_data = Pointer;
 
 struct image_delayload_descriptor
 {
@@ -18,19 +14,19 @@ struct image_delayload_descriptor
     std::uint32_t all_attributes;
 
     //RVA to the name of the target library (NULL-terminate ASCII string)
-    std::uint32_t name;
+    rva_type name;
     //RVA to the HMODULE caching location (PHMODULE)
-    std::uint32_t module_handle_rva;
+    rva_type module_handle_rva;
     //RVA to the start of the IAT (image_thunk_data)
-    std::uint32_t address_table;
+    rva_type address_table;
     //RVA to the start of the name table (image_thunk_data::address_of_data)
-    std::uint32_t lookup_table;
+    rva_type lookup_table;
     //RVA to an optional bound IAT
-    std::uint32_t bound_import_address_table_rva;
+    rva_type bound_import_address_table_rva;
     //RVA to an optional unload info table
-    std::uint32_t unload_information_table_rva;
+    rva_type unload_information_table_rva;
     //0 if not bound, otherwise, date/time of the target DLL
-    std::uint32_t time_date_stamp;
+    rva_type time_date_stamp;
 };
 
 } //namespace pe_bliss::detail::delay_import
