79e7890a759f3feed0d3dffc78508ab60c73a0d2
Denis T <dragon-dreamer@users.noreply.github.com>
2023-06-11 19:37:21 +0100

    Update cmake build
    - Add static/shared libs build support
    - Add static/shared MSVC runtime link support

---
+++ cmake/output_options.cmake
@@ -0,0 +1,9 @@
+macro(set_output_dirs target_name)
+	set_target_properties("${target_name}"
+		PROPERTIES
+		RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+		LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+		ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+		PDB_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+		COMPILE_PDB_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
+endmacro()
