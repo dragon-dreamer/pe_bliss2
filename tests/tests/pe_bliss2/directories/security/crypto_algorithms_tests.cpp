1e94ec29d22720a3e80e1f59b820855bdc9271f0
Denis T <dragon-dreamer@users.noreply.github.com>
2023-12-03 20:01:24 +0000

    Add get_hash_and_signature_algorithms unit tests

--- tests/tests/pe_bliss2/directories/security/crypto_algorithms_tests.cpp
+++ tests/tests/pe_bliss2/directories/security/crypto_algorithms_tests.cpp
@@ -2,7 +2,12 @@
 
 #include "gtest/gtest.h"
 
+#include "pe_bliss2/error_list.h"
+#include "pe_bliss2/security/pkcs7/signer_info.h"
+
 #include "simple_asn1/crypto/algorithms.h"
+
+#include "tests/pe_bliss2/pe_error_helper.h"
 
 using namespace pe_bliss::security;
 
@@ -72,4 +77,99 @@ TEST(CryptoAlgorithmsTests, AlgorithmIdEqualsSpan)
 	ASSERT_FALSE(algorithm_id_equals(alg1, alg2));
 	ASSERT_FALSE(algorithm_id_equals(alg1, alg3));
 	ASSERT_FALSE(algorithm_id_equals(alg3, alg2));
+}
+
+TEST(GetHashAndSignatureAlgorithmsTest, BothInvalid)
+{
+	digest_algorithm digest_alg;
+	digest_encryption_algorithm digest_encryption_alg;
+	pkcs7::signer_info_pkcs7<vector_range_type> signer_info{};
+	pe_bliss::error_list errors;
+	ASSERT_FALSE(get_hash_and_signature_algorithms(signer_info, digest_alg,
+		digest_encryption_alg, errors));
+
+	expect_contains_errors(errors,
+		crypto_algorithm_errc::unsupported_digest_algorithm,
+		crypto_algorithm_errc::unsupported_digest_encryption_algorithm);
+	ASSERT_EQ(digest_alg, digest_algorithm::unknown);
+	ASSERT_EQ(digest_encryption_alg, digest_encryption_algorithm::unknown);
+}
+
+TEST(GetHashAndSignatureAlgorithmsTest, DigestAlgoInvalid)
+{
+	digest_algorithm digest_alg;
+	digest_encryption_algorithm digest_encryption_alg;
+	pkcs7::signer_info_pkcs7<vector_range_type> signer_info{};
+	signer_info.get_underlying().digest_algorithm.algorithm.container.assign(
+		asn1::crypto::hash::id_sha256.begin(),
+		asn1::crypto::hash::id_sha256.end());
+	pe_bliss::error_list errors;
+	ASSERT_FALSE(get_hash_and_signature_algorithms(signer_info, digest_alg,
+		digest_encryption_alg, errors));
+
+	expect_contains_errors(errors,
+		crypto_algorithm_errc::unsupported_digest_encryption_algorithm);
+	ASSERT_EQ(digest_alg, digest_algorithm::sha256);
+	ASSERT_EQ(digest_encryption_alg, digest_encryption_algorithm::unknown);
+}
+
+TEST(GetHashAndSignatureAlgorithmsTest, Valid)
+{
+	digest_algorithm digest_alg;
+	digest_encryption_algorithm digest_encryption_alg;
+	pkcs7::signer_info_pkcs7<vector_range_type> signer_info{};
+	signer_info.get_underlying().digest_algorithm.algorithm.container.assign(
+		asn1::crypto::hash::id_sha256.begin(),
+		asn1::crypto::hash::id_sha256.end());
+	signer_info.get_underlying().digest_encryption_algorithm.algorithm.container.assign(
+		asn1::crypto::pki::id_rsa.begin(),
+		asn1::crypto::pki::id_rsa.end());
+	pe_bliss::error_list errors;
+	ASSERT_TRUE(get_hash_and_signature_algorithms(signer_info, digest_alg,
+		digest_encryption_alg, errors));
+
+	expect_contains_errors(errors);
+	ASSERT_EQ(digest_alg, digest_algorithm::sha256);
+	ASSERT_EQ(digest_encryption_alg, digest_encryption_algorithm::rsa);
+}
+
+TEST(GetHashAndSignatureAlgorithmsTest, ValidMismatch)
+{
+	digest_algorithm digest_alg;
+	digest_encryption_algorithm digest_encryption_alg;
+	pkcs7::signer_info_pkcs7<vector_range_type> signer_info{};
+	signer_info.get_underlying().digest_algorithm.algorithm.container.assign(
+		asn1::crypto::hash::id_sha256.begin(),
+		asn1::crypto::hash::id_sha256.end());
+	signer_info.get_underlying().digest_encryption_algorithm.algorithm.container.assign(
+		asn1::crypto::signature::id_sha384_with_rsa_encryption.begin(),
+		asn1::crypto::signature::id_sha384_with_rsa_encryption.end());
+	pe_bliss::error_list errors;
+	ASSERT_FALSE(get_hash_and_signature_algorithms(signer_info, digest_alg,
+		digest_encryption_alg, errors));
+
+	expect_contains_errors(errors,
+		crypto_algorithm_errc::signature_hash_and_digest_algorithm_mismatch);
+	ASSERT_EQ(digest_alg, digest_algorithm::sha256);
+	ASSERT_EQ(digest_encryption_alg, digest_encryption_algorithm::rsa);
+}
+
+TEST(GetHashAndSignatureAlgorithmsTest, ValidMatch)
+{
+	digest_algorithm digest_alg;
+	digest_encryption_algorithm digest_encryption_alg;
+	pkcs7::signer_info_pkcs7<vector_range_type> signer_info{};
+	signer_info.get_underlying().digest_algorithm.algorithm.container.assign(
+		asn1::crypto::hash::id_sha384.begin(),
+		asn1::crypto::hash::id_sha384.end());
+	signer_info.get_underlying().digest_encryption_algorithm.algorithm.container.assign(
+		asn1::crypto::signature::id_sha384_with_rsa_encryption.begin(),
+		asn1::crypto::signature::id_sha384_with_rsa_encryption.end());
+	pe_bliss::error_list errors;
+	ASSERT_TRUE(get_hash_and_signature_algorithms(signer_info, digest_alg,
+		digest_encryption_alg, errors));
+
+	expect_contains_errors(errors);
+	ASSERT_EQ(digest_alg, digest_algorithm::sha384);
+	ASSERT_EQ(digest_encryption_alg, digest_encryption_algorithm::rsa);
 }
