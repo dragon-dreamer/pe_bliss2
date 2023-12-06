d514f04cda587b9b1cfcc5d731a296ee26b9b5d4
Denis T <dragon-dreamer@users.noreply.github.com>
2023-12-06 22:31:14 +0000

    Move Authenticode timestamp signature check status struct to a separate header

--- pe_bliss2/include/pe_bliss2/security/authenticode_timestamp_signature_verifier.h
+++ pe_bliss2/include/pe_bliss2/security/authenticode_timestamp_signature_verifier.h
@@ -11,6 +11,7 @@
 #include "pe_bliss2/pe_error.h"
 #include "pe_bliss2/security/authenticode_certificate_store.h"
 #include "pe_bliss2/security/authenticode_timestamp_signature.h"
+#include "pe_bliss2/security/authenticode_timestamp_signature_check_status.h"
 #include "pe_bliss2/security/authenticode_timestamp_signature_format_validator.h"
 #include "pe_bliss2/security/buffer_hash.h"
 #include "pe_bliss2/security/byte_range_types.h"
@@ -24,40 +25,12 @@
 #include "pe_bliss2/security/x509/x509_certificate_store.h"
 
 #include "simple_asn1/types.h"
+#include "simple_asn1/crypto/crypto_common_types.h"
 
 #include "utilities/variant_helpers.h"
 
 namespace pe_bliss::security
 {
-
-template<typename RangeType>
-struct [[nodiscard]] timestamp_signature_check_status
-{
-	error_list authenticode_format_errors;
-	error_list certificate_store_warnings;
-	std::optional<bool> hash_valid;
-	std::optional<bool> message_digest_valid;
-	std::optional<digest_algorithm> digest_alg;
-	std::optional<digest_algorithm> imprint_digest_alg;
-	std::optional<digest_encryption_algorithm> digest_encryption_alg;
-	std::optional<signature_verification_result> signature_result;
-	std::variant<std::monostate, asn1::utc_time, asn1::generalized_time> signing_time;
-
-	std::optional<x509::x509_certificate_store<
-		x509::x509_certificate_ref<RangeType>>> cert_store;
-
-	[[nodiscard]]
-	explicit operator bool() const noexcept
-	{
-		return !authenticode_format_errors.has_errors()
-			&& hash_valid
-			&& *hash_valid
-			&& (!message_digest_valid || *message_digest_valid)
-			&& signature_result
-			&& *signature_result
-			&& !std::holds_alternative<std::monostate>(signing_time);
-	}
-};
 
 namespace impl
 {
